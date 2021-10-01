3bdd4368bde1036c96e6ca48f8fc31a8bd2eec70
dixondeuxyeux <dixondeuxyeux@gmail.com>
2021-10-01 18:51:14 +0000

    module

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 module.exports = {
-  reactStrictMode: true,
-  images: { domains: ['res.cloudinary.com'] },
-}
-
-module.exports = {
   webpack5: true,
   webpack: (config) => {
     config.resolve.fallback = { fs: false }
